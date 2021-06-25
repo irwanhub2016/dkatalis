9d2bbcfb4b90cc1e155fb19a6888facb7c89fa02
Irwan Syarifudin <irwan.syarifudin@sleekr.co>
2021-06-25 09:30:15 +0700

    add log and update class name

--- API/spec/api_spec.rb
+++ API/spec/api_spec.rb
@@ -1,5 +1,5 @@
 require 'api_base'
-require_relative "../lib/json_compare"
+require_relative "../lib/json_comparison"
 
 include JsonComparison
 
@@ -10,23 +10,24 @@ describe ApiBase do
   	end
 
 	it 'Test API with two JSON data for single loop' do
-		result_json_1 = ApiBase.send_api(@url_target, @request_method, rand(1...10).to_s)
-		result_json_2 = ApiBase.send_api(@url_target, @request_method, rand(1...10).to_s)
+		# test api with random user id as parameter in endpoint. For the exist user data only 1-12
+		result_json_1 = ApiBase.send_api(@url_target, @request_method, rand(1...12).to_s)
+		result_json_2 = ApiBase.send_api(@url_target, @request_method, rand(1...12).to_s)
 
 		if ApiBase.validate_id(result_json_1['data']['id'],result_json_2['data']['id'])
 	 		expect(compare_json(result_json_1,result_json_2)).to be true
 	 	else
 	 		expect(compare_json(result_json_1,result_json_2)).to be false
 	 	end
+
+	 	p 'success run the first test scenario'
 	end
 
 	it "Test API with two JSON data for #{ENV['LOOP']} loop" do
-		result_json_1 = ApiBase.send_api(@url_target, @request_method, rand(1...10).to_s)
-		result_json_2 = ApiBase.send_api(@url_target, @request_method, rand(1...10).to_s)
-
+		# test api with random user id as parameter in endpoint. For the exist user data only 1-12 
 	 	ENV['LOOP'].to_i.times do
-			result_json_1 = ApiBase.send_api(@url_target, @request_method, rand(1...10).to_s)
-			result_json_2 = ApiBase.send_api(@url_target, @request_method, rand(1...10).to_s)
+			result_json_1 = ApiBase.send_api(@url_target, @request_method, rand(1...12).to_s)
+			result_json_2 = ApiBase.send_api(@url_target, @request_method, rand(1...12).to_s)
 
 			if ApiBase.validate_id(result_json_1['data']['id'],result_json_2['data']['id'])
 		 		expect(compare_json(result_json_1,result_json_2)).to be true
@@ -36,17 +37,23 @@ describe ApiBase do
 
 		 	p "Compare request user ID #{result_json_1['data']['id']} and #{result_json_2['data']['id']}"
 		end
+
+		p 'success run the second test scenario'
 	end
 
 	it 'Test API with empty JSON data' do
 		result_json_1 = []
 		result_json_2 = []
 		expect { compare_json(result_json_1,result_json_2) }.to raise_error(TypeError)
+
+		p 'success run the third test scenario'
 	end
 
 	it 'Test API with nil JSON data' do
 		result_json_1 = ""
 		result_json_2 = ""
 		expect { compare_json(result_json_1,result_json_2) }.to raise_error(TypeError)
+
+		p 'success run the fourth test scenario'
 	end
 end
