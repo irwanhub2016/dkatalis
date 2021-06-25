ef6837fd3a789db4fd1c1262f66d386df9af21a0
Irwan Syarifudin <irwan.syarifudin@sleekr.co>
2021-06-25 11:39:00 +0700

    add static test

--- API/spec/api_spec.rb
+++ API/spec/api_spec.rb
@@ -9,6 +9,16 @@ describe ApiBase do
 		@request_method = "Get"
   	end
 
+	it 'Test API with two JSON data for static user id' do
+		result_json_1 = ApiBase.send_api(@url_target, @request_method, "2")
+		result_json_2 = ApiBase.send_api(@url_target, @request_method, "2")
+
+	 	expect(boolean=compare_json(result_json_1,result_json_2)).to be true
+
+	 	p "Compare request user ID #{result_json_1['data']['id']} and #{result_json_2['data']['id']}: #{boolean}"
+	 	p "success run the first test scenario #{boolean}"
+	end
+
 	it 'Test API with two JSON data for single loop' do
 		# test api with random user id as parameter in endpoint. For the exist user data only 1-12
 		result_json_1 = ApiBase.send_api(@url_target, @request_method, rand(1...12).to_s)
